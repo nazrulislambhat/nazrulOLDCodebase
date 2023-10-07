d94e0d21251fd74f69848ad13cab0784c41506e2
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-07 11:13:03 +0530

    main navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,9 @@
+'use client';
 import Logo from './logo';
 import Nav from './nav';
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-[20%] py-8 text-center">
+    <header className="flex justify-between px-4 py-6 sm:px-6 sm:py-8 xl:px-8 xl:py-10  items-center bg-primary">
       <Logo />
       <Nav />
     </header>
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,8 @@
+'use client';
 import Link from 'next/link';
 import { Silkscreen } from 'next/font/google';
+import { useState } from 'react';
+import { CgMenuRight, CgClose } from 'react-icons/cg';
 
 const silk = Silkscreen({
   weight: '400',
@@ -7,22 +10,50 @@ const silk = Silkscreen({
 });
 
 export default function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className={`${silk.className} font-bold text-xl uppercase`}>
-      <ul className="flex gap-8 items-center ">
-        <li className="bg-lightGrey rounded-full hover:bg-primary hover:text-secondary hover:underline px-6 py-2 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="bg-lightGrey rounded-full hover:bg-primary hover:text-secondary hover:underline px-6 py-2 cursor-pointer underline-offset-4">
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li className="bg-lightGrey rounded-full hover:bg-primary hover:text-secondary hover:underline px-6 py-2 cursor-pointer underline-offset-4">
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li className="bg-lightGrey rounded-full hover:bg-primary hover:text-secondary hover:underline px-6 py-2 cursor-pointer underline-offset-4">
-          <Link href="/privacy">Privacy</Link>
-        </li>
-      </ul>
+    <div className="relative">
+      <button
+        className="z-10 text-4xl text-secondary flex items-center justify-center"
+        onClick={toggleMenu}
+      >
+        {menuOpen ? <CgClose /> : <CgMenuRight />}
+      </button>
+
+      {menuOpen && (
+        <div
+          className={`${silk.className} absolute right-0 w-[16rem] bg-white sm:w-[20rem] overflow-y-hidden overflow-x-hidden  `}
+        >
+          <ul className="flex gap-2 flex-col items-center justify-center py-4">
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/">Home</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/about">About</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/portfolio">Portfolio</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/privacy">Privacy</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/faqs">FAQs</Link>
+            </li>
+            <li className=" hover:bg-primary hover:text-secondary max-w-fit py-2 px-4 cursor-pointer underline-offset-4 active:bg-red active:text-lightGrey">
+              <Link href="/terms">Terms</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
