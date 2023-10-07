203e672c045dbc38dc3339bbb5b7616b6e718e69
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-07 20:52:02 +0530

    hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import logoSrc from '../public/home-image.webp';
+import logoSrc from '../public/logo5transparent.webp';
 import Image from 'next/image';
 import { Instrument_Sans, Silkscreen } from 'next/font/google';
 import Link from 'next/link';
@@ -14,26 +14,26 @@ export default function Hero() {
     <div className="relative bg-primary flex flex-col items-center">
       <div className="flex gap-2 flex-col text-center justify-center items-center py-12 px-12 xl:px-24 xl:py-24 2xl:px-56 2xl:py-24 ">
         <h1
-          className={`${silk.className} text-[2rem] md:text-[3rem] xl:text-[3.5rem] 2xl:text-[4rem] text-secondary`}
+          className={`${silk.className} text-[2.5rem] md:text-[3rem] xl:text-[3.5rem] 2xl:text-[4rem] text-secondary`}
         >
           Full Stack Engineer
         </h1>
 
         <p
-          className={`${instrument.className} text-lighterGrey text-[.8em] md:text-[1rem] xl:text-[1.1rem] 2xl:text-[1.2rem] `}
+          className={`${instrument.className} text-lighterGrey text-[.9em] md:text-[1rem] xl:text-[1.1rem] 2xl:text-[1.2rem] `}
         >
           I have a passion for developing comprehensive and scalable full-stack
           products that deliver outstanding user experiences.
         </p>
       </div>
 
-      <div className="relative ">
+      <div className="relative w-[70%] xl:w-[100vw]  flex justify-center ">
         <Image
           src={logoSrc}
           alt="Nazrul Islam Photo"
           width={512}
           height={512}
-          className="relative -bottom-12 z-6 flex justify-center rounded-full"
+          className="relative z-6"
         />
       </div>
     </div>
