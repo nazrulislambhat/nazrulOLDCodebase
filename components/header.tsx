3a4e923bcc873effac0ad62302065c1be81174cf
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-08 12:33:34 +0530

    hero

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import logoSrc from '../public/logo5transparent.webp';
 import Nav from './nav';
 export default function Header() {
   return (
-    <header className="flex justify-between relative bg-primary pl-12 xl:pl-24 2xl:pl-56">
+    <header className="flex justify-between relative items-center bg-primary px-12 xl:px-24 2xl:px-56">
       <div className="logo  items-center bg-primary  py-4">
         <Image
           src={logoSrc}
@@ -15,7 +15,7 @@ export default function Header() {
         />
       </div>
 
-      <div className="bg-primary flex w-[30vw] justify-end pr-12 xl:pr-24 2xl:pr-56 items-center xl:bg-secondary">
+      <div className="nav">
         <Nav />
       </div>
     </header>
