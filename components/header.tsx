210bf90b70c772ec88ebc7e2cfaeeb4193b91192
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-08 14:47:51 +0530

    hero

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,18 @@
 'use client';
-import Image from 'next/image';
-import logoSrc from '../public/logo5transparent.webp';
 import Nav from './nav';
 import Link from 'next/link';
-import { Silkscreen } from 'next/font/google';
+import { Instrument_Sans, Bai_Jamjuree } from 'next/font/google';
 
-const silk = Silkscreen({
-  weight: '400',
-  subsets: ['latin'],
-});
+const instrument = Instrument_Sans({ subsets: ['latin'] });
+const jam500 = Bai_Jamjuree({ subsets: ['latin'], weight: '500' });
+const jam700 = Bai_Jamjuree({ subsets: ['latin'], weight: '700' });
 
 export default function Header() {
   return (
     <header className="flex justify-between relative items-center bg-primary px-12 xl:px-24 2xl:px-56">
       <div className="logo  items-center bg-primary  py-4">
         <Link href="/" passHref>
-          <p
-            className={`${silk.className} text-4xl text-bg-gradient`}
-          >
+          <p className={`${jam700.className} text-3xl xl:text-4xl text-bg-gradient`}>
             NAZRUL
           </p>{' '}
         </Link>
