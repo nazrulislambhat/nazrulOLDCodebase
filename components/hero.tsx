e90abd4887302554bb3c887302483457e04726b8
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-08 13:05:27 +0530

    hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,7 +11,7 @@ const silk = Silkscreen({
 
 export default function Hero() {
   return (
-    <div className="relative bg-primary flex flex-col items-center pt-24 pb-2 px-12 xl:px-24 xl:pt-24 2xl:px-56 2xl:pt-24">
+    <div className="relative bg-primary flex flex-col h-[100%] md:h-[100%] pb-2 overflow-hidden items-center pt-24 px-12 xl:px-24 xl:pt-24 2xl:px-56 2xl:pt-24">
       <div className="flex gap-2 flex-col text-center justify-center items-center pb-12 ">
         <h1
           className={`${silk.className} text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl text-secondary pb-2`}
