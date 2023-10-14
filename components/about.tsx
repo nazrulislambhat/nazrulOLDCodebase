612addfad17e4d505953db707d5c079634240ce7
Nazrul Islam <nazrulislambhat@gmail.com>
2023-10-14 21:03:06 +0530

    About Component

--- components/about.tsx
+++ components/about.tsx
@@ -11,11 +11,11 @@ const inter = Inter_Tight({ subsets: ['latin'] });
 export default function About() {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 100 }}
+      initial={{ opacity: 0, y: 500 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ type: 'tween', duration: 2 }}
-      className={`relative rounded-xl border-2 border-terinary my-4 bg-secondary h-fit justify-between gap-12 xl:gap-24 px-6 py-24 sm:px-8 sm:py-24 md:py-30 lg:px-16 lg:py-36 xl:px-24 xl:py-36 flex flex-col xl:flex-row items-center overflow-hidden`}
+      className={`relative xl:rounded-xl border-y-2 xl:border-2 border-terinary my-4 bg-secondary h-fit justify-between gap-12 xl:gap-24 px-6 py-24 sm:px-8 sm:py-24 md:py-30 lg:px-16 lg:py-36 xl:px-24 xl:py-36 flex flex-col xl:flex-row items-center overflow-hidden`}
     >
       <div className="left bg-primary rounded-xl">
         <Image
